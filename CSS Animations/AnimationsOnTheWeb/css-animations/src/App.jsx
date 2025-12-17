2e40d8a9940325c4f2cd171c19623fba24a721f0
cmvinayak <cmvinayak04@gmail.com>
2025-12-17 18:03:33 +0530

    Added CSS animations/animations on the web and played around with css animations.

---
+++ CSS Animations/AnimationsOnTheWeb/css-animations/src/App.jsx
@@ -0,0 +1,13 @@
+import Box from './components/box.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Box />
+    </>
+  )
+}
+
+export default App
---
+++ CSS Animations/AnimationsOnTheWeb/css-animations/src/components/box.jsx
@@ -0,0 +1,14 @@
+export default function Box() {
+  return (
+    <>
+    <section className="box-container">
+      <div className="box-1"></div>
+      <div className="box-2"></div>
+      <div className="box-3"></div>
+      <div className="box-4"></div>
+      <div className="box-5"></div>
+
+    </section>  
+    </>
+  )
+}
